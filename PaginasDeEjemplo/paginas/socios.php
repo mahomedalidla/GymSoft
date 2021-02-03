5c2f05ec8c896d8c8ad0746ee681207c32c5cc60
Mahomedalid Aguirre <mahomedalidla@gmail.com>
2021-02-03 14:46:04 -0700

    Paginas de emplo para proyecto y Requisitos

---
+++ PaginasDeEjemplo/paginas/socios.php
@@ -0,0 +1,375 @@
+<?php
+  include('includes/encabezado.php');
+?>
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Socios</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body p-0">
+          <table class="table table-striped projects">
+              <thead>
+                  <tr>
+                      <th style="width: 1%">
+                          #
+                      </th>
+                      <th style="width: 25%">
+                          Nombre de Socio
+                      </th>
+                      <th style="width: 20%">
+                          Tipo de Membresia
+                      </th>
+                      <th>
+                          Progreso
+                      </th>
+                      <th style="width: 8%" class="text-center">
+                          Estado
+                      </th>
+                      <th style="width: 25%">
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>
+                          1
+                      </td>
+
+                      <td>
+                          <a>
+                              Bryan Mahomedalid Lopez Aguirre
+                          </a>
+                          <br/>
+                          <small>
+                              Registrado el 28.01.2021
+                          </small>
+                      </td>
+                      <td>
+                          <a>
+                              Estudiante
+                          </a>
+                      </td>
+                      <td class="project_progress">
+                          <div class="progress progress-sm">
+                              <div class="progress-bar bg-green" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 5%">
+                              </div>
+                          </div>
+                          <small>
+                              57% Complete
+                          </small>
+                      </td>
+                      <td class="project-state">
+                          <span class="badge badge-success">Activo</span>
+                      </td>
+                      <td class="project-actions text-right">
+                          <a class="btn btn-primary btn-sm" href="#">
+                              <i class="fas fa-folder">
+                              </i>
+                              Ver
+                          </a>
+                          <a class="btn btn-info btn-sm" href="#">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Editar
+                          </a>
+                          <a class="btn btn-danger btn-sm" href="#">
+                              <i class="fas fa-trash">
+                              </i>
+                              Borrar
+                          </a>
+                      </td>
+                  </tr>
+                  <tr>
+
+
+
+                  <tr>
+                      <td>
+                          1
+                      </td>
+
+                      <td>
+                          <a>
+                              Bryan Mahomedalid Lopez Aguirre
+                          </a>
+                          <br/>
+                          <small>
+                              Registrado el 28.01.2021
+                          </small>
+                      </td>
+                      <td>
+                          <a>
+                              Estudiante
+                          </a>
+                      </td>
+                      <td class="project_progress">
+                          <div class="progress progress-sm">
+                              <div class="progress-bar bg-green" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 5%">
+                              </div>
+                          </div>
+                          <small>
+                              57% Complete
+                          </small>
+                      </td>
+                      <td class="project-state">
+                          <span class="badge badge-success">Activo</span>
+                      </td>
+                      <td class="project-actions text-right">
+                          <a class="btn btn-primary btn-sm" href="#">
+                              <i class="fas fa-folder">
+                              </i>
+                              Ver
+                          </a>
+                          <a class="btn btn-info btn-sm" href="#">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Editar
+                          </a>
+                          <a class="btn btn-danger btn-sm" href="#">
+                              <i class="fas fa-trash">
+                              </i>
+                              Borrar
+                          </a>
+                      </td>
+                  </tr>
+                  <tr>
+
+
+                  <tr>
+                      <td>
+                          1
+                      </td>
+
+                      <td>
+                          <a>
+                              Bryan Mahomedalid Lopez Aguirre
+                          </a>
+                          <br/>
+                          <small>
+                              Registrado el 28.01.2021
+                          </small>
+                      </td>
+                      <td>
+                          <a>
+                              Estudiante
+                          </a>
+                      </td>
+                      <td class="project_progress">
+                          <div class="progress progress-sm">
+                              <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 95%">
+                              </div>
+                          </div>
+                          <small>
+                              95% Complete
+                          </small>
+                      </td>
+                      <td class="project-state">
+                          <span class="badge badge-warning">Proximo</span>
+                      </td>
+                      <td class="project-actions text-right">
+                          <a class="btn btn-primary btn-sm" href="#">
+                              <i class="fas fa-folder">
+                              </i>
+                              Ver
+                          </a>
+                          <a class="btn btn-info btn-sm" href="#">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Editar
+                          </a>
+                          <a class="btn btn-danger btn-sm" href="#">
+                              <i class="fas fa-trash">
+                              </i>
+                              Borrar
+                          </a>
+                      </td>
+                  </tr>
+                  <tr>
+
+
+
+                  <tr>
+                      <td>
+                          1
+                      </td>
+
+                      <td>
+                          <a>
+                              Bryan Mahomedalid Lopez Aguirre
+                          </a>
+                          <br/>
+                          <small>
+                              Registrado el 28.01.2021
+                          </small>
+                      </td>
+                      <td>
+                          <a>
+                              Estudiante
+                          </a>
+                      </td>
+                      <td class="project_progress">
+                          <div class="progress progress-sm">
+                              <div class="progress-bar bg-green" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 5%">
+                              </div>
+                          </div>
+                          <small>
+                              57% Complete
+                          </small>
+                      </td>
+                      <td class="project-state">
+                          <span class="badge badge-success">Activo</span>
+                      </td>
+                      <td class="project-actions text-right">
+                          <a class="btn btn-primary btn-sm" href="#">
+                              <i class="fas fa-folder">
+                              </i>
+                              Ver
+                          </a>
+                          <a class="btn btn-info btn-sm" href="#">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Editar
+                          </a>
+                          <a class="btn btn-danger btn-sm" href="#">
+                              <i class="fas fa-trash">
+                              </i>
+                              Borrar
+                          </a>
+                      </td>
+                  </tr>
+                  <tr>
+
+
+
+                  <tr>
+                      <td>
+                          1
+                      </td>
+
+                      <td>
+                          <a>
+                              Bryan Mahomedalid Lopez Aguirre
+                          </a>
+                          <br/>
+                          <small>
+                              Registrado el 28.01.2021
+                          </small>
+                      </td>
+                      <td>
+                          <a>
+                              Estudiante
+                          </a>
+                      </td>
+                      <td class="project_progress">
+                          <div class="progress progress-sm">
+                              <div class="progress-bar bg-green" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 5%">
+                              </div>
+                          </div>
+                          <small>
+                              57% Complete
+                          </small>
+                      </td>
+                      <td class="project-state">
+                          <span class="badge badge-success">Activo</span>
+                      </td>
+                      <td class="project-actions text-right">
+                          <a class="btn btn-primary btn-sm" href="#">
+                              <i class="fas fa-folder">
+                              </i>
+                              Ver
+                          </a>
+                          <a class="btn btn-info btn-sm" href="#">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Editar
+                          </a>
+                          <a class="btn btn-danger btn-sm" href="#">
+                              <i class="fas fa-trash">
+                              </i>
+                              Borrar
+                          </a>
+                      </td>
+                  </tr>
+                  <tr>
+
+
+
+                  <tr>
+                      <td>
+                          1
+                      </td>
+
+                      <td>
+                          <a>
+                              Bryan Mahomedalid Lopez Aguirre
+                          </a>
+                          <br/>
+                          <small>
+                              Registrado el 28.01.2021
+                          </small>
+                      </td>
+                      <td>
+                          <a>
+                              Estudiante
+                          </a>
+                      </td>
+                      <td class="project_progress">
+                          <div class="progress progress-sm">
+                              <div class="progress-bar bg-red" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+                              </div>
+                          </div>
+                          <small>
+                              100% Complete
+                          </small>
+                      </td>
+                      <td class="project-state">
+                          <span class="badge badge-danger">Vencido</span>
+                      </td>
+                      <td class="project-actions text-right">
+                          <a class="btn btn-primary btn-sm" href="#">
+                              <i class="fas fa-folder">
+                              </i>
+                              Ver
+                          </a>
+                          <a class="btn btn-info btn-sm" href="#">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Editar
+                          </a>
+                          <a class="btn btn-danger btn-sm" href="#">
+                              <i class="fas fa-trash">
+                              </i>
+                              Borrar
+                          </a>
+                      </td>
+                  </tr>
+                  <tr>
+
+              </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <?php
+    include('includes/pie.php');
+  ?>
